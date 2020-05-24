e7539e162575b06ba8b0bfefa041e5cb3eafb0da
rdm-x <65864736+rdm-x@users.noreply.github.com>
2020-05-24 23:51:56 +0530

    Update filter_syntax.py

--- filter_syntax.py
+++ filter_syntax.py
@@ -1,19 +1,19 @@
 # -----------------------------------------------------------------------------
 
-# filter -> filters items out of a sequence and returns a filtered list
+n = [1, 2, 3, 4]
 
-n = [1,2,3,4]
 
 def fltr(lst):
-    lst2 = [x for x in lst if x>2]
+    lst2 = [x for x in lst if x > 2]
     return lst2
+
 
 print(fltr(n))
 
 # or
 
-print(list(filter(lambda x:x>2,n)))
+print(list(filter(lambda x: x > 2, n)))
 
-# list comprehension solution 
+# list comprehension solution
 
-print([x for x in n if x>2])
+print([x for x in n if x > 2])
