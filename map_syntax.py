a40e69b7d04dbfb4bc7c437d05fa7462c2664f6a
rdm-x <65864736+rdm-x@users.noreply.github.com>
2020-05-24 23:53:53 +0530

    Update map_syntax.py

--- map_syntax.py
+++ map_syntax.py
@@ -2,20 +2,22 @@
 
 # map -> apply same function to each element of a sequence and return the modified list
 
-n=[1,2,3,4]
+n = [1, 2, 3, 4]
+
 
 def square(lst1):
-    lst2=[]
+    lst2 = []
     for num in lst1:
         lst2.append(num**2)
     return lst2
+
 
 print(square(n))
 
 # or
 
-print(list(map(lambda x:x**2,n)))
+print(list(map(lambda x: x**2, n)))
 
-# list comprehension solution 
+# list comprehension solution
 
 print([x**2 for x in n])
