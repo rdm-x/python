0a4ba999a89ec32a58633a3f75e53ac59b707a3f
rdm-x <65864736+rdm-x@users.noreply.github.com>
2020-05-24 23:18:44 +0530

    Update lambda_syntax.py

--- lambda_syntax.py
+++ lambda_syntax.py
@@ -1,3 +1,6 @@
+# -----------------------------------------------------------------------------
+
+
 # Lamda funtion to multiply two given numbers
 
 def multiply(x,y):
@@ -8,3 +11,19 @@ def multiply(x,y):
 multiply=lambda x,y:x*y
 
 print(multiply(3,2))
+
+# -----------------------------------------------------------------------------
+
+# Lamda funtion to multiply two given numbers
+
+def compare(x,y):
+    if x>y:
+        return x
+    else:
+        return y
+ 
+# or
+
+compare=lamdba x,y:x if x>y else y
+ 
+print(compare(4,5)
