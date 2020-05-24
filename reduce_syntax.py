f7df5523d4c1924205801703dd9f8a890c01f9c8
rdm-x <65864736+rdm-x@users.noreply.github.com>
2020-05-24 23:47:28 +0530

    Create reduce_syntax.py

---
+++ reduce_syntax.py
@@ -0,0 +1,24 @@
+# -----------------------------------------------------------------------------
+
+# reduce -> applies same operation to items of a sequence 
+# uses result of operation as first parameter of next operation and returns an item not a list
+
+n=[1,2,3,4]
+
+def mult(lst):
+    prod=lst[0]
+    for i in range(1,len(lst)):
+        prod*=lst[i]
+    return prod
+
+print(mult(n))
+
+# result is 24
+# 1*2 = 2
+# 2*3 = 6
+# 6*4 = 24
+
+
+# or 
+
+print(reduce(lambda x,y:x*y,n))
